192fe2356f3b3c1bcda95f4afbe2f1be52b87ce3
leslieowititech <leslieowiti@yahoo.com>
2021-07-19 19:25:45 -0400

    Fixed some bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 import configureStore from './store';
+
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
 
 function Root() {
   return (
-    <ReduxProvider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ReduxProvider>
+    </Provider>
   );
 }
 
@@ -26,5 +29,6 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
