5d0d5dbb19b8cd0642d6a0f512459c14067ec86e
leslieowititech <leslieowiti@yahoo.com>
2021-07-19 14:21:21 -0400

    Finished user auth and sessions

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,15 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+// const { restoreUser } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
 
 module.exports = router;
