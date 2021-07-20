76bb16b976bc417e40889bed5e3979db1c031372
leslieowititech <leslieowiti@yahoo.com>
2021-07-20 19:14:18 -0400

    Moving on to signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import {Route, Switch} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 import { Header } from "./components/Header";
-import LoginFormPage from "./components/LoginFormPage";
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  
+  return  isLoaded && (
     <div>
       <Header />
       <Switch>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import './Header.css';
 
 export const Header = () => {
+  const history = useHistory();
+
+  const reddirect = () => {
+    history.push('/login')
+  }
+
+
     return (
         <div className="header">
-          <div>logo</div>  
+          <div className="logo">logo</div>  
           <div className="middle-nav-div">
             <nav className="middle-nav">
               <li className="header-li">Hair Spots</li>
@@ -16,7 +24,10 @@ export const Header = () => {
               <li className="header-li">List your Spot</li>
               <li className="header-li">🌐</li>
               <li className="header-li">
-                <button className="nav-button">
+                <button 
+                     className="nav-button"
+                     onClick={reddirect}
+                >
                   <div>&#9776;</div>
                   <div className="profileIcon-div">
                     <img src="images/profileIcon.jpg" alt="profileImageIcon" className="profile-icon"/>
