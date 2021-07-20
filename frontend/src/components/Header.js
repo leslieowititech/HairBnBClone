62301a64045e90eab70975c499dcc9d8b8fddb19
leslieowititech <leslieowiti@yahoo.com>
2021-07-20 18:20:25 -0400

    Finished loginForm page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,8 +4,27 @@ import './Header.css';
 export const Header = () => {
     return (
         <div className="header">
-          <div>logo</div>         
-          <div>&#9776;</div>                 
-        </div>
+          <div>logo</div>  
+          <div className="middle-nav-div">
+            <nav className="middle-nav">
+              <li className="header-li">Hair Spots</li>
+              <li className="header-li">Experiences</li>
+            </nav>
+          </div>       
+          <div className="right-nav-div">
+            <nav className="right-nav">
+              <li className="header-li">List your Spot</li>
+              <li className="header-li">🌐</li>
+              <li className="header-li">
+                <button className="nav-button">
+                  <div>&#9776;</div>
+                  <div className="profileIcon-div">
+                    <img src="images/profileIcon.jpg" alt="profileImageIcon" className="profile-icon"/>
+                  </div>
+                </button>
+               </li>
+            </nav>
+          </div>                 
+          </div>
     )
 }
