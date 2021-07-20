86357253be3f2e0eaa3b5466c4734a3e97b15ee3
leslieowititech <leslieowiti@yahoo.com>
2021-07-19 22:25:51 -0400

    Debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
+
+
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
