0d9d692de8676b8fcfd9dc2b1d5bf65f29c13804
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-07-21 18:38:36 -0400

    Figuring out the signin signup button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
+import LoginFormPage from "./components/LoginFormModal";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
@@ -22,14 +22,14 @@ function App() {
   return  isLoaded && (
     <div>
       <Navigation isLoaded={isLoaded} />
-      <Header />
+      <Header isLoaded={isLoaded}/>
       <BigImage/>
       {isLoaded && (
         <Switch>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-          <Route path="/signup">
+          <Route path="/">
             <LoginFormPage />
           </Route>
         </Switch>
---
+++ frontend/src/components/Header/LoginSignup/LoginSignup.js
@@ -0,0 +1,7 @@
+const LoginSignup = () => {
+    return (
+        <div className="login-signup-div"></div>
+    )
+}
+
+export default LoginSignup;
