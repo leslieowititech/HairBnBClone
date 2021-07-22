35c8dbc3e55184e91a90399a55246b4e76fa080d
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-07-22 18:06:43 -0400

    debugging

--- backend/routes/api/locations.js
+++ backend/routes/api/locations.js
@@ -1,3 +1,11 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { Location } = require('../../db/models/location');
 
 const router = express.Router();
+
+router.get('/', asyncHandler( async (req,res) => {
+    const loactions = await Location.findPlaces();
+
+    return loactions;
+}))
