d47b5c6cb64cdf318b2d5fa4b369dc6ee8a2f799
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-07-23 22:46:00 -0400

    demo user is logging in

--- frontend/src/components/Header/LoginSignup/index.js
+++ frontend/src/components/Header/LoginSignup/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+
+import * as sessionActions from '../../../store/session'
 import LoginFormModal from '../../LoginFormModal';
 import ProfileButton from '../../Navigation/ProfileButton';
 
 import './LoginSignup.css';
 
 const LoginSignup = ({isLoaded}) => {
+    const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState("demo@mail.com");
+    const [password, setPassword] = useState("password");
+    const [errors, setErrors] = useState([]);
+
+   
+    const logDemoIn = () => {   
+        setErrors([]);
+        return dispatch(sessionActions.login({ credential, password })).catch(
+            async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            }
+        );
+    };
 
     let sessionLinks;
     if (sessionUser) {
@@ -29,8 +46,8 @@ const LoginSignup = ({isLoaded}) => {
         <div className="login-signup-div">
             <div className='login-signup-demo-links'>
                 
-                    {isLoaded && sessionLinks}
-                <li>Demo User</li>
+                {isLoaded && sessionLinks}
+                <li><button className='demo-user-button' onClick={logDemoIn}>Demo User</button></li>
             </div>
             <div className='login-signup-help-list-yourspot'>
                 <li>List your spot</li>
