0b2c371f4e509e14fa10762b8ff06667f56470c4
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-09-09 12:51:29 -0400

    typo

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
   Booking.associate = function(models) {
     // associations can be defined here
     Booking.belongsTo(models.Location, { foreignKey: 'locationId' })
-    Booking.belongsTo(models.User, { foreignKey: 'userId' })
+    Booking.belongsTo(models.User, {foreignKey: 'userId'})
   };
   return Booking;
 };
