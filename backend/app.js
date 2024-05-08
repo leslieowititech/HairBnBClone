ed0f72c8af63e6a5da8ca187dab19e4eb9194fe6
leslieowiti <leslieowiti@yahoo.com>
2024-05-08 13:19:41 -0400

    starting to make updates

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,42 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const csurf = require('csurf');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
 
-const { environment } = require('./config');
-const isProduction = environment === 'production';
-const { ValidationError } = require('sequelize');
+const { environment } = require("./config");
+const isProduction = environment === "production";
+const { ValidationError } = require("sequelize");
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
-  // helmet helps set a variety of headers to better secure your app
-  app.use(helmet({
-    contentSecurityPolicy: false
-  }));
-  
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
-      },
-    })
-  );
+  // enable cors only in development
+  app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
-const routes = require('./routes');
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+const routes = require("./routes");
 
 app.use(routes); // Connect all the routes
 
@@ -45,12 +47,12 @@ app.use((_req, _res, next) => {
   err.errors = ["The requested resource couldn't be found."];
   err.status = 404;
   next(err);
-});// Process sequelize errors
+}); // Process sequelize errors
 app.use((err, _req, _res, next) => {
   // check if error is a Sequelize error:
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
-    err.title = 'Validation error';
+    err.title = "Validation error";
   }
   next(err);
 });
@@ -59,13 +61,11 @@ app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
   res.json({
-    title: err.title || 'Server Error',
+    title: err.title || "Server Error",
     message: err.message,
     errors: err.errors,
     stack: isProduction ? null : err.stack,
   });
 });
 
-
 module.exports = app;
-
