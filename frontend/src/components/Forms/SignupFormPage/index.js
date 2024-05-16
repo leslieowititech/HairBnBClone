024cad1fd5a729e7cd0e8689c8a87f6df7114c5e
leslieowiti <leslieowiti@yahoo.com>
2024-05-16 15:08:03 -0400

    updated imports to match updated npms in frontend

--- frontend/src/components/Forms/SignupFormPage/index.js
+++ frontend/src/components/Forms/SignupFormPage/index.js
@@ -1,10 +1,10 @@
 // frontend/src/components/SignupFormPage/index.js
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import * as sessionActions from "../../../store/session";
 
-import './SignupForm.css'
+import "./SignupForm.css";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -15,71 +15,72 @@ function SignupFormPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
+  if (sessionUser) return <Navigate to="/" replace />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
       setErrors([]);
-      return dispatch(sessionActions.signup({ email, username, password }))
-        .catch(async (res) => {
-          const data = await res.json();
-          if (data && data.errors) setErrors(data.errors);
-        });
+      return dispatch(
+        sessionActions.signup({ email, username, password })
+      ).catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
     }
-    return setErrors(['Confirm Password field must be the same as the Password field']);
+    return setErrors([
+      "Confirm Password field must be the same as the Password field",
+    ]);
   };
 
   return (
-    
-
-    <form onSubmit={handleSubmit} className='signup-form'>
+    <form onSubmit={handleSubmit} className="signup-form">
       <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>    
-        <h1 className='log-in-form-h1'>Welcome to hairbnb</h1>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder='Email'
-          className='signup-form-input'
-        />
-      
-      
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          placeholder='Username'
-          className='signup-form-input'
-        />
-      
-      
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          placeholder='Password'
-          className='signup-form-input'
-        />
-      
-      
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-          placeholder='confirm password'
-          className='signup-form-input'
-        />
-      
-      <button type="submit" className='signup-form-button'>Sign Up</button>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <h1 className="log-in-form-h1">Welcome to hairbnb</h1>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Email"
+        className="signup-form-input"
+      />
+
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+        placeholder="Username"
+        className="signup-form-input"
+      />
+
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        placeholder="Password"
+        className="signup-form-input"
+      />
+
+      <input
+        type="password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        required
+        placeholder="confirm password"
+        className="signup-form-input"
+      />
+
+      <button type="submit" className="signup-form-button">
+        Sign Up
+      </button>
     </form>
-    
   );
 }
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,35 +1,38 @@
 // frontend/src/store/index.js
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
+} from "redux";
+import { thunk } from "redux-thunk";
 
-import sessionReducer from './session';
-import locationReducer from './location';
-import imageReducer from './image';
-import bookingsReducer from './booking';
-import '../index.css';
+import sessionReducer from "./session";
+import locationReducer from "./location";
+import imageReducer from "./image";
+import bookingsReducer from "./booking";
+import "../index.css";
 
 const rootReducer = combineReducers({
-    session: sessionReducer,
-    location: locationReducer,
-    image: imageReducer,
-    booking: bookingsReducer,
+  session: sessionReducer,
+  location: locationReducer,
+  image: imageReducer,
+  booking: bookingsReducer,
 });
 
 let enhancer;
 
-
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-
 }
 
 const configureStore = (preloadedState) => {
-    return createStore(rootReducer, preloadedState, enhancer);
-  };
-  
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
 export default configureStore;
